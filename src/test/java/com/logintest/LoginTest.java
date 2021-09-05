23c6841c17175875087351bce2aae6fc81c59e4c
ramesh dodle <dodle@adobe.com>
2021-09-05 17:27:29 +0530

    first push

---
+++ src/test/java/com/logintest/LoginTest.java
@@ -0,0 +1,20 @@
+package com.logintest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    @Test
+    public void loginTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.adobe.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+        String title=driver.getTitle();
+    }
+}
