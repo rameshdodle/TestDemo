8ff2fac92e67c20fc64591961575afc3b55c4997
ramesh dodle <dodle@adobe.com>
2021-09-05 23:57:23 +0530

    title commented

--- src/test/java/com/logintest/LoginTest.java
+++ src/test/java/com/logintest/LoginTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
+
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
@@ -15,6 +17,6 @@ public class LoginTest {
         driver.get("https://www.adobe.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
-        String title=driver.getTitle();
+        //String title=driver.getTitle();
     }
 }
