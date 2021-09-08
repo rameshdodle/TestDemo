cb4a3a849fb5e41a16645357081e28092094d52f
ramesh dodle <dodle@adobe.com>
2021-09-08 11:51:38 +0530

    title Uncommented

--- src/test/java/com/logintest/LoginTest.java
+++ src/test/java/com/logintest/LoginTest.java
@@ -8,15 +8,24 @@ import org.testng.annotations.Test;
 
 
 import java.util.concurrent.TimeUnit;
+import io.percy.selenium.Percy;
 
 public class LoginTest {
+
+
     @Test
-    public void launchPageURL(){
+    public void launchPageURL() throws InterruptedException {
+        Percy percy;
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
+        percy = new Percy(driver);
         driver.get("https://www.adobe.com/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
-        //String title=driver.getTitle();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String title=driver.getTitle();
+        percy.snapshot("Demo");
+        Thread.sleep(3000);
+
+                driver.quit();
     }
 }
